1bc9b67805a788c801a1e14507e2514f9fe9dd13
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-01 22:21:29 +0100

    feat: create media

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
 import GetInvolved from "../components/ui/GetInvolved";
-import MediaList from "../components/ui/MediaList";
 
 const Home = () => (
   <div className="App">
     <GetInvolved />
-    <MediaList />
   </div>
 );
 
