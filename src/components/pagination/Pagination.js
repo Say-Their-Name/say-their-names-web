c3bd750d4049e8f2bd0079946a201ef3a5ef8752
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-03 18:16:10 +0100

    feat: pagination functionality

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,72 @@
+import React from "react";
+import ReactPaginate from "react-paginate";
+import styled from "styled-components";
+
+const StyledPagination = styled.div`
+  .pagination {
+    margin: 15px auto;
+    display: flex;
+    list-style: none;
+    outline: none;
+  }
+  .pagination > .active > a {
+    background-color: black;
+    border-color: black;
+    color: #fff;
+  }
+  .pagination > li > a {
+    border: 1px solid black;
+    padding: 5px 10px;
+    outline: none;
+    cursor: pointer;
+  }
+  .pagination > .active > a,
+  .pagination > .active > span,
+  .pagination > .active > a:hover,
+  .pagination > .active > span:hover,
+  .pagination > .active > a:focus,
+  .pagination > .active > span:focus {
+    background-color: black;
+    border-color: black;
+    outline: none;
+  }
+  .pagination > li > a,
+  .pagination > li > span {
+    color: black;
+  }
+  .pagination > li:first-child > a,
+  .pagination > li:first-child > span,
+  .pagination > li:last-child > a,
+  .pagination > li:last-child > span {
+    border-radius: unset;
+  }
+`;
+const Pagination = ({ paginationData, fetchdata }) => {
+  console.log(paginationData);
+  const { total, last_page } = paginationData;
+
+  const onChange = (touched) => {
+    fetchdata(touched.selected);
+    console.log(touched.selected);
+  };
+
+  return (
+    <StyledPagination>
+      <ReactPaginate
+        previousLabel={"<"}
+        nextLabel={">"}
+        breakLabel={"..."}
+        breakClassName={"break-me"}
+        pageCount={last_page}
+        marginPagesDisplayed={1}
+        // pageRangeDisplayed={5}
+        onPageChange={onChange}
+        containerClassName={"pagination"}
+        subContainerClassName={"pages pagination"}
+        activeClassName={"active"}
+      />
+    </StyledPagination>
+  );
+};
+
+export default Pagination;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
 
-
-import GetInvolved from '../components/ui/getInvolved/GetInvolved';
-import ProfileList from '../components/ui/profileList/ProfileList';
-import Spinner from '../components/common/Spinner';
-import config from '../utils/config';
+import GetInvolved from "../components/ui/getInvolved/GetInvolved";
+import ProfileList from "../components/ui/profileList/ProfileList";
+import Spinner from "../components/common/Spinner";
+import Pagination from "../components/pagination/Pagination";
+import config from "../utils/config";
 
 const { apiBaseUrl } = config;
 
 const Home = () => {
   const [profiles, setProfiles] = useState([]); // this will hold the profles list fetched from the API
   const [loading, setLoading] = useState(true);
+  const [paginationData, setPaginationData] = useState({});
+
+  const fetchdata = useCallback(async (page = 1) => {
+    console.log("aaa", page);
+    try {
+      const response = await axios.get(`${apiBaseUrl}/people?page=${page}`);
+      // console.log(response.data.links);
+      setPaginationData(response.data.meta);
+      setProfiles(response.data.data);
+    } catch (error) {
+      // toast(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
   useEffect(() => {
-    const fetchdata = async () => {
-      try {
-        const response = await axios.get(`${apiBaseUrl}/people`);
-        setProfiles(response.data.data);
-      } catch (error) {
-        // toast(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchdata();
-  }, []);
+  }, [fetchdata]);
 
   return (
     <div className="App">
@@ -33,7 +38,10 @@ const Home = () => {
       {loading ? (
         <Spinner height="40vh" />
       ) : (
-        <ProfileList profiles={profiles} />
+        <>
+          <ProfileList profiles={profiles} />
+          <Pagination paginationData={paginationData} fetchdata={fetchdata} />
+        </>
       )}
     </div>
   );
