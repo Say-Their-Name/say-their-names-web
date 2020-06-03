5f340d1410a117461bf5862945e8ddad1d6967cf
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-03 19:00:05 +0100

    feat: pagination functionality

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,81 +1,41 @@
-import React from "react";
-import ReactPaginate from "react-paginate";
-import styled from "styled-components";
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+import PropTypes from 'prop-types';
 
-const StyledPagination = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
+import Container from '../common/Container';
+import StyledPagination from './styles';
 
-  .pagination {
-    margin: 15px auto;
-    display: flex;
-    list-style: none;
-    outline: none;
-  }
-  .pagination > .active > a {
-    background-color: black;
-    border-color: black;
-    color: #fff;
-  }
-  .pagination > li > a {
-    border: 1px solid black;
-    padding: 5px 10px;
-    outline: none;
-    cursor: pointer;
-
-    &:hover {
-      background-color: black;
-      color: white;
-    }
-  }
-  .pagination > .active > a,
-  .pagination > .active > span,
-  .pagination > .active > a:hover,
-  .pagination > .active > span:hover,
-  .pagination > .active > a:focus,
-  .pagination > .active > span:focus {
-    background-color: black;
-    border-color: black;
-    outline: none;
-  }
-  .pagination > li > a,
-  .pagination > li > span {
-    color: black;
-  }
-  .pagination > li:first-child > a,
-  .pagination > li:first-child > span,
-  .pagination > li:last-child > a,
-  .pagination > li:last-child > span {
-    border-radius: unset;
-  }
-`;
 const Pagination = ({ paginationData, fetchdata }) => {
-  console.log(paginationData);
-  const { total, last_page } = paginationData;
+  const { last_page } = paginationData;
 
   const onChange = (touched) => {
     fetchdata(touched.selected + 1);
-    // console.log(touched.selected + 1);
   };
 
   return (
-    <StyledPagination>
-      <ReactPaginate
-        previousLabel={"<"}
-        nextLabel={">"}
-        breakLabel={"..."}
-        breakClassName={"break-me"}
-        pageCount={last_page}
-        // marginPagesDisplayed={1}
-        // pageRangeDisplayed={5}
-        onPageChange={onChange}
-        containerClassName={"pagination"}
-        subContainerClassName={"pages pagination"}
-        activeClassName={"active"}
-      />
-    </StyledPagination>
+    <Container>
+      <StyledPagination>
+        <ReactPaginate
+          previousLabel={'<'}
+          nextLabel={'>'}
+          breakLabel="..."
+          breakClassName="break-me"
+          pageCount={last_page}
+          onPageChange={onChange}
+          containerClassName="pagination"
+          subContainerClassName="pages pagination"
+          activeClassName="active"
+        />
+      </StyledPagination>
+    </Container>
   );
 };
 
 export default Pagination;
+
+Pagination.propTypes = {
+  paginationData: PropTypes.shape({
+    last_page: PropTypes.string.isRequired
+  }).isRequired,
+  fetchdata: PropTypes.func.isRequired
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "react-toastify/dist/ReactToastify.css";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
-import App from "./App";
-import GlobalStyle from "./styles";
-import * as serviceWorker from "./serviceWorker";
+import App from './App';
+import GlobalStyle from './styles';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,7 +15,7 @@ ReactDOM.render(
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
