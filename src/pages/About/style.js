98d6d3e378c59f6b4b6f52ef3c7b5ecc5c32879c
Utiee Obot <Utibeobot2001@gmail.com>
2020-06-02 23:25:51 -0500

    feat:about

--- src/pages/About/style.js
+++ src/pages/About/style.js
@@ -2,44 +2,32 @@ import styled from 'styled-components';
 import Color from '../../constants/Color';
 
 const StyledAbout = styled.div`
- 
   line-height: 1.6;
   font-weight: normal;
   margin: auto;
   background-color: ${Color.WHITE};
   text-align: center;
   width: 50%;
-  padding: 5rem 3rem;
- 
 
+  padding: 5rem 3rem;
   h2 {
     margin-top: 0;
     text-align: left;
     font-weight: 'bold';
-    
-
   }
-  
 `;
 
-
 const StyledAboutContainer = styled.div`
-
-
-  h3 {
-   
+  h3 { 
   font-weight: 'bold';
   text-align: left;
-
   }
-
   p{
     margin-left: auto;
     margin-right: auto;
     color:#333;
     word-wrap:break-word;
-    text-align: justify;
-    
+    text-align: justify;  
   }
   hr{
     padding: 0px;
@@ -48,13 +36,9 @@ const StyledAboutContainer = styled.div`
   a:hover: {
     textDecoration: 'underline',
   }
- 
 `;
 
-
 export {
-
   StyledAbout,
   StyledAboutContainer
-
 };
