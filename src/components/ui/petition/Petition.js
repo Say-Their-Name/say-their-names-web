fc9a24093eed689bac4c2a878c435a445cc50b5b
jdshaeffer <shaefferjd@gmail.com>
2020-06-03 23:32:44 -0600

    feat: petition cards, preparations for real petition data

---
+++ src/components/ui/petition/Petition.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { PetitionCard, PetitionLink } from './styles';
+
+const Petition = ({
+  title, description, link
+}) => (
+  <PetitionCard>
+    <div style={{ height: '40%', backgroundColor: 'black' }}>
+      {/* <img src={imgUrl} alt={title} /> */}
+    </div>
+    <div style={{display: 'flex', flexDirection: 'column', alignContent: 'space-between'}}>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <PetitionLink href={link}>FIND OUT MORE</PetitionLink>
+    </div>
+  </PetitionCard>
+);
+
+Petition.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  link: PropTypes.string,
+  imgUrl: PropTypes.string
+};
+
+Petition.defaultProps = {
+  title: 'title',
+  description: 'description',
+  link: 'link',
+  imgUrl: 'imgUrl'
+};
+
+export default Petition;
--- src/pages/Petitions.js
+++ src/pages/Petitions.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Petition from '../components/ui/petition/Petition';
+import { PetitionWrapper } from '../components/ui/petition/styles';
 
-const Petitions = () => (
-  <h1>Petitions</h1>
-);
+const Petitions = () => {
+  const [petitions, setPetitions] = useState([]);
+
+  const fetchPetitions = async () => {
+    const res = await fetch('https://saytheirnames.dev/api/petitions');
+    const json = await res.json();
+    setPetitions(json.data);
+  };
+
+  useEffect(() => {
+    fetchPetitions();
+  }, []);
+
+  return (
+    <PetitionWrapper>
+      {petitions.map((petition) => (
+        <Petition
+          key={petition.id}
+          title={petition.title}
+          description={petition.description}
+          link={petition.link}
+        />
+      ))}
+    </PetitionWrapper>
+  );
+};
 
 export default Petitions;
