cfdff5921f17c4d9fa9ce1b9afaefb3fb803e835
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-05 09:32:30 +0100

    add required styles for navbar

--- src/components/navigation/NavigationItem.js
+++ src/components/navigation/NavigationItem.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-import { StyledNavigationLinks } from "./styles";
+import { StyledNavigationLinks } from './styles';
 
 const NavigationItem = ({ name, path }) => {
   const location = useLocation();
-  let active = "";
+  let active = '';
   if (path === location.pathname) {
-    active = "active";
+    active = 'active';
   }
 
   return (
@@ -24,5 +24,5 @@ export default NavigationItem;
 
 NavigationItem.propTypes = {
   name: PropTypes.string.isRequired,
-  path: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired
 };
