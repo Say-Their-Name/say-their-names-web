97f57755b66b56658c2b51da6071215ec13fcdf9
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-06 17:08:24 +0100

    ui fixes

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import Container from '../../components/common/Container';
+import Container from "../../components/common/Container";
 import {
   ContainerDiv,
   ImageCover,
   HomeButton,
   BoxContent,
-  InnerContainer
-} from './style';
-import BLM from '../../assets/blm.svg';
+  InnerContainer,
+  ImageContainer,
+} from "./style";
+import BLM from "../../assets/blm.svg";
 
 const NotFound = ({ message, longMessage }) => (
   <Container>
     <ContainerDiv>
       <InnerContainer>
         <BoxContent>
-          <span>
-            404
-            {' '}
-            <br />
-            {message || ' Page not found'}
-          </span>
+          <h1>404</h1> <br />
+          <h4>{message || " Page not found"}</h4>
         </BoxContent>
         <p>
-          {longMessage
-            || 'Not all those who wander are lost, but it seems you may have taken a wrong turn.'}
+          {longMessage ||
+            "Not all those who wander are lost, but it seems you may have taken a wrong turn."}
         </p>
         <HomeButton>
           <Link to="/">
@@ -34,7 +31,9 @@ const NotFound = ({ message, longMessage }) => (
           </Link>
         </HomeButton>
       </InnerContainer>
-      <ImageCover src={BLM} alt="not found  svg" />
+      <ImageContainer>
+        <ImageCover src={BLM} alt="not found  svg" />
+      </ImageContainer>
     </ContainerDiv>
   </Container>
 );
@@ -43,11 +42,11 @@ export default NotFound;
 
 NotFound.propTypes = {
   message: PropTypes.string,
-  longMessage: PropTypes.string
+  longMessage: PropTypes.string,
 };
 
 NotFound.defaultProps = {
-  message: ' Page not found',
+  message: " Page not found",
   longMessage:
-    'Not all those who wander are lost, but it seems you may have taken a wrong turn.'
+    "Not all those who wander are lost, but it seems you may have taken a wrong turn.",
 };
--- src/pages/notFound/style.js
+++ src/pages/notFound/style.js
@@ -1,14 +1,18 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import Color from "../../constants/Color";
 
 const ContainerDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+  justify-content: center;
   -webkit-box-align: center;
   align-items: center;
-  width: 1024px;
-  margin: 24px auto 0px;
+  width: 100%;
   min-height: 80vh;
+
+  @media (max-width: 750px) {
+    height: auto;
+  }
 
   p {
     margin-bottom: 36px;
@@ -16,33 +20,70 @@ const ContainerDiv = styled.div`
   }
 `;
 
+const ImageContainer = styled.div`
+  width: 45%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const InnerContainer = styled.div`
+  width: 45%;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (max-width: 700px) {
+    width: 100%;
+    align-items: center;
+  }
+`;
+
 const ImageCover = styled.img`
   width: 550px;
   height: 476px;
-  margin-left: 24px;
-  margin-bottom: -20px;
+
+  @media (max-width: 700px) {
+    display: none;
+  }
 `;
 
 const HomeButton = styled.article`
   button {
-    width: 162px;
-    height: 36px;
     border: 1px solid black;
-    color: #040d14;
-    background-color: white;
+    color: ${Color.WHITE};
+    background-color: ${Color.PRIMARY};
+    font-size: 1rem;
+    width: 300px;
     cursor: pointer;
+    padding: 1rem;
     outline: none;
-    transition: all 0.2s ease-in-out;
+    transition: all 0.4s ease-in-out;
     :hover {
       opacity: 0.7;
       transform: translateY(-2px);
+      color: ${Color.PRIMARY};
+      background-color: ${Color.WHITE};
+    }
+
+    @media (max-width: 500px) {
+      width: auto;
     }
   }
 `;
 
 const BoxContent = styled.div`
   margin-bottom: 24px;
-  span {
+  h1 {
+    font-size: 6rem;
+    margin: 0;
+
+    @media (max-width: 700px) {
+      font-size: 4rem;
+    }
+  }
+  h4 {
     color: #040d14;
     font-family: "Capsule Sans Display", system-ui, -apple-system,
       BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
@@ -52,10 +93,15 @@ const BoxContent = styled.div`
     line-height: 40px;
     letter-spacing: -0.1px;
     text-decoration: unset;
+    margin: 0;
   }
 `;
 
-const InnerContainer = styled.div``;
 export {
-  ContainerDiv, ImageCover, HomeButton, BoxContent, InnerContainer
+  ContainerDiv,
+  ImageCover,
+  ImageContainer,
+  HomeButton,
+  BoxContent,
+  InnerContainer,
 };
