2004a46cae7e9b13f5b2b6e13410ba600447c1db
Utiee Obot <utibeobot2001@gmail.com>
2020-06-08 01:13:55 -0500

    webview-about update (#72)
    * webview-about update
    
    * chore: fixed about-mobile
    
    * chore: added back the spacing
    
    Co-authored-by: Kouame <kndri@redventures.com>
    Co-authored-by: Kouame N'Dri <56895441+kndri@users.noreply.github.com>
    Co-authored-by: Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Routes from './routes';
+import Routes from './routes/index';
 
 import { trackPageView, initializeGA } from './utils/gaTracker';
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import About from '../components/about/About';
+
+const AboutPage = () => (
+
+  <About />
+
+);
+export default AboutPage;
