51a651e49a5469f9047108d281b9ac02b62b25dd
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-08 11:21:35 +0100

    add meta tags

--- src/components/common/Seo.js
+++ src/components/common/Seo.js
@@ -11,7 +11,7 @@ const Seo = () => (
     />
 
     {/* Open Graph / Facebook - */}
-    {/* <meta property="og:type" content="website" />
+    <meta property="og:type" content="website" />
       <meta property="og:url" content="https://metatags.io/" />
       <meta
         property="og:title"
@@ -24,7 +24,7 @@ const Seo = () => (
       <meta
         property="og:image"
         content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-      /> */}
+      />
 
     {/* Twitter  */}
     {/* <meta property="twitter:card" content="summary_large_image" />
