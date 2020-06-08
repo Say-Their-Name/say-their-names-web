2839cb09516e41cf22238b4b4537d51c6a72e0d8
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-08 11:30:14 +0100

    add meta tags

--- src/components/common/Seo.js
+++ src/components/common/Seo.js
@@ -1,42 +1,41 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from "react";
+import Helmet from "react-helmet";
+import Favicon from "../../assets/logo.png";
 
 const Seo = () => (
   <Helmet>
     {/* <title>Meta Tags — Preview, Edit and Generate</title> */}
     <meta name="title" content="Profiles" />
-    <meta
-      name="description"
-      content="Profiles"
-    />
+    <meta name="description" content="Profiles" />
 
     {/* Open Graph / Facebook - */}
     <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" />
-      <meta
-        property="og:title"
-        content="Meta Tags — Preview, Edit and Generate"
-      />
-      <meta
-        property="og:description"
-        content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
-      />
-      <meta
-        property="og:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-      />
+    <meta property="og:url" content="https://metatags.io/" />
+    <meta
+      property="og:title"
+      content="Meta Tags — Preview, Edit and Generate"
+    />
+    <meta
+      property="og:description"
+      content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+    />
+    <meta property="og:image" content={Favicon} />
 
     {/* Twitter  */}
-    {/* <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io/" />
-      <meta
-        property="twitter:title"
-        content="Meta Tags — Preview, Edit and Generate"
-      />
-      <meta
-        property="twitter:description"
-        content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
-      /> */}
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://metatags.io/" />
+    <meta
+      property="twitter:title"
+      content="Meta Tags — Preview, Edit and Generate"
+    />
+    <meta
+      property="twitter:description"
+      content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+    />
+    <meta
+      property="twitter:image"
+      content={Favicon}
+    />
   </Helmet>
 );
 
