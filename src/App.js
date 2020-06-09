e85ce0081b8a66a3543b11860a9c7927bb93de28
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-09 20:20:33 +0100

    metatags fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import Routes from './routes/index';
-import Seo from './components/common/Seo';
 import { trackPageView, initializeGA } from './utils/gaTracker';
 
 initializeGA();
@@ -17,12 +16,10 @@ const App = () => {
 
   return (
     <div className="App">
-      <Seo
-        title="Home"
-        image="https://say-their-names.fra1.cdn.digitaloceanspaces.com/assets/stn-logo.png"
-        description="Our aim is to build an open-source platform that raises awareness of the injustice and often forgotten names of racial inequality"
+      <Routes
+        initialLoadProp={initialLoad}
+        setInitialLoadProp={() => setInitialLoad(true)}
       />
-      <Routes initialLoadProp={initialLoad} setInitialLoadProp={() => setInitialLoad(true)} />
     </div>
   );
 };
