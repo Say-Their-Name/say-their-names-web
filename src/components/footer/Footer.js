c037890b7fa64bd3fb029050886dcf9a8ab1f2ac
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-13 13:25:12 +0100

    implement feedback

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -48,7 +48,7 @@ const Footer = () => (
         <StyledFooter width="25%">
           <h2>
             DOWNLOADS
-            <span>(Coming shortly)</span>
+            <span> (Coming shortly)</span>
           </h2>
           <p>
             Download the app
